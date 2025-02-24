9b4bbbbf18a43aa1593d17ecbc5f354f9c705c92
Siavash Namvar <snsinahub@github.com>
2025-02-24 18:12:54 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const github = require('@actions/github')
 const core = require('@actions/core')
 const _ = require('lodash')
 const fs = require('fs')
-const replace =
+const replace = require('./utils/replace')
 
 async function run() {
     try {
