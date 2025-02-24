9d85f9fe9c81f479c1a8a138ebb60a0ef5932328
Siavash Namvar <snsinahub@github.com>
2025-02-24 18:10:17 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const github = require('@actions/github')
 const core = require('@actions/core')
-const graphql = require('@octokit/graphql')
-const rest = require('@octokit/rest')
 const _ = require('lodash')
 const fs = require('fs')
 const replace =
