e29dda2b1df601dc9cdbd0e96d3d6538f0f1ebca
Siavash Namvar <snsinahub@github.com>
2025-02-25 14:04:38 -0500

    modify the code

--- src/utils/replace.js
+++ src/utils/replace.js
@@ -6,7 +6,6 @@ module.exports = class ReplaceString {
     }
 
     replace(string, pattern, replacement, flags = '') {
-        console.log("INPUTS:", string, pattern, replacement, flags);
         let result;
 
         try {
@@ -17,17 +16,13 @@ module.exports = class ReplaceString {
                     // Add 'i' flag for case-insensitive matching
                     const regExp = new RegExp(safePattern, flags + 'i');
                     result = string.replace(regExp, replacement);
-                    console.log('Result after regex replacement:', result);
                 } catch (regexError) {
-                    console.error('Invalid regex pattern, performing simple string replacement:', regexError);
                     // If pattern is not a valid regex, perform simple string replacement
                     result = string.split(new RegExp(pattern, 'i')).join(replacement);
-                    console.log('Result after simple string replacement:', result);
                 }
             } else {
                 // Perform simple string replacement
                 result = string.split(new RegExp(pattern, 'i')).join(replacement);
-                console.log('Result after simple string replacement:', result);
             }
         } catch (error) {
             console.error('Error during replacement:', error);
