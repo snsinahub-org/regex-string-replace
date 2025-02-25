e51e789c90a6024604081672339a6a212a8340bf
Siavash Namvar <snsinahub@github.com>
2025-02-25 10:14:27 -0500

    update index.js

--- src/utils/replace.js
+++ src/utils/replace.js
@@ -1,24 +1,29 @@
 
 module.exports = class ReplaceString {
-  constructor() {
-    this.replace = this.replace.bind(this)
-  }
-
-  replace(string, regex, replacement, flags) {
-    console.log(string, regex, replacement, flags);
-    if (regex) {
-      
-      // Perform regex replacement
-      const regExp = new RegExp(regex, flags);
-      
-      const result = string.replace(regExp, replacement);
-      console.log('Result after regex replacement:', result);
-      return result;
-    } else {
-      // Perform simple string replacement
-      const result = string.split(regex).join(replacement);
-      console.log('Performing simple string replacement', result);
-      return result;
-    }
+    constructor() {
+        this.replace = this.replace.bind(this);
+      }
+    
+      replace(string, pattern, replacement, flags) {
+        console.log("INPUTS:", string, pattern, replacement, flags);
+        let result;
+    
+        try {
+          if (pattern) {
+            // Perform regex replacement
+            const regExp = new RegExp(pattern, flags);
+            result = string.replace(regExp, replacement);
+            console.log('Result after regex replacement:', result, regExp);
+          } else {
+            // Perform simple string replacement
+            result = string.split(pattern).join(replacement);
+            console.log('Performing simple string replacement', result);
+          }
+        } catch (error) {
+          console.error('Error during replacement:', error);
+          result = string;
+        }
+    
+        return result;
   }
 }
