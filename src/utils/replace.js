9fa1f52b9e21f1b287785d56bb52da7e10cf8b45
Siavash Namvar <snsinahub@github.com>
2025-02-25 10:20:32 -0500

    update index.js

--- src/utils/replace.js
+++ src/utils/replace.js
@@ -2,28 +2,34 @@
 module.exports = class ReplaceString {
     constructor() {
         this.replace = this.replace.bind(this);
-      }
-    
-      replace(string, pattern, replacement, flags) {
+    }
+
+    replace(string, pattern, replacement, flags) {
         console.log("INPUTS:", string, pattern, replacement, flags);
         let result;
-    
+
         try {
-          if (pattern) {
-            // Perform regex replacement
-            const regExp = new RegExp(pattern, flags);
-            result = string.replace(regExp, replacement);
-            console.log('Result after regex replacement:', result, regExp);
-          } else {
-            // Perform simple string replacement
-            result = string.split(pattern).join(replacement);
-            console.log('Performing simple string replacement', result);
-          }
+            if (pattern) {
+                // Check if the pattern is a valid regex
+                try {
+                    const regExp = new RegExp(pattern, flags);
+                    result = string.replace(regExp, replacement);
+                    console.log('Result after regex replacement:', result);
+                } catch (regexError) {
+                    // If pattern is not a valid regex, perform simple string replacement
+                    result = string.split(pattern).join(replacement);
+                    console.log('Performing simple string replacement', result);
+                }
+            } else {
+                // Perform simple string replacement
+                result = string.split(pattern).join(replacement);
+                console.log('Performing simple string replacement', result);
+            }
         } catch (error) {
-          console.error('Error during replacement:', error);
-          result = string;
+            console.error('Error during replacement:', error);
+            result = string;
         }
-    
+
         return result;
-  }
+    }
 }
