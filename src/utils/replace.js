e274ce9070b5b861526a4a0fbeb3fd7b5a3ee43f
Siavash Namvar <snsinahub@github.com>
2025-02-25 11:51:53 -0500

    update index.js

--- src/utils/replace.js
+++ src/utils/replace.js
@@ -3,26 +3,27 @@ module.exports = class ReplaceString {
         this.replace = this.replace.bind(this);
     }
 
-    replace(string, pattern, replacement, flags) {
+    replace(string, pattern, replacement, flags = '') {
         console.log("INPUTS:", string, pattern, replacement, flags);
         let result;
 
         try {
             if (pattern) {
                 try {
-                    // Check if the pattern is a valid regex
-                    const regExp = new RegExp(pattern, flags);
+                    // Add 'i' flag for case-insensitive matching
+                    const regExp = new RegExp(pattern, flags + 'i');
                     result = string.replace(regExp, replacement);
                     console.log('Result after regex replacement:', result);
                 } catch (regexError) {
+                    console.error('Invalid regex pattern, performing simple string replacement:', regexError);
                     // If pattern is not a valid regex, perform simple string replacement
-                    result = string.split(pattern).join(replacement);
-                    console.log('Performing simple string replacement', result);
+                    result = string.split(new RegExp(pattern, 'i')).join(replacement);
+                    console.log('Result after simple string replacement:', result);
                 }
             } else {
                 // Perform simple string replacement
-                result = string.split(pattern).join(replacement);
-                console.log('Performing simple string replacement', result);
+                result = string.split(new RegExp(pattern, 'i')).join(replacement);
+                console.log('Result after simple string replacement:', result);
             }
         } catch (error) {
             console.error('Error during replacement:', error);
